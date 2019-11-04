dd8b8b382b1b96fc31583f93cf7c0c95602e5b4c
boulicheDjedjiga <56534901+boulicheDjedjiga@users.noreply.github.com>
2019-11-04 11:41:52 +0100

    TP2

---
+++ tp2/script.js
@@ -0,0 +1,30 @@
+function validation ( )
+{
+    document.getElementById("resultat").setAttribute("hidden",'hidden');
+    document.getElementById("error").setAttribute("hidden",'hidden');
+
+    if (document.getElementById("nom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }  
+     else if(document.getElementById("prenom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le prenom doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+      
+     else if(document.getElementById("adresse").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "L'adresse doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     else if(document.getElementById("mail").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractères !";
+         document.getElementById("error").removeAttribute("hidden");
+    }    
+     else {
+        document.getElementById("resultat").removeAttribute("hidden");
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value
+
+     }
+
+ }
+
