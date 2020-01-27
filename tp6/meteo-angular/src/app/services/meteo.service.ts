c7a909593621a1a6158ecfa150f219b2559e00c8
boulicheDjedjiga <56534901+boulicheDjedjiga@users.noreply.github.com>
2020-01-28 00:03:53 +0100

    meteo

---
+++ tp6/meteo-angular/src/app/services/meteo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { MeteoItem } from '../meteoItem';
+
+@Injectable({ providedIn: 'root' })
+export class MeteoService {
+
+  constructor() { }
+
+
+  getMeteo(name: string): Promise<any> {
+    console.log('from service', name);
+
+    let m = new MeteoItem();
+
+    return fetch('https://api.openweathermap.org/data/2.5/weather/?q=' + name + '&units=metric&lang=fr&appid=20c56898a8442969cb75873af6a8670f')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+
+        // test du code retour
+        // 200 = OK
+        // 404 = city not found 
+        if (json.cod === 200) {
+          return Promise.resolve(json);
+        } else {
+          m.weather = json;
+
+          console.error('Météo introuvable pour ' + name
+            + ' (' + json.message + ')');
+
+          return Promise.reject('Météo introuvable pour ' + name
+          + ' (' + json.message + ')');
+        }
+
+      });
+
+  }
+}
